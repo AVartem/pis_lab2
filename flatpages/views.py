09b503f84fd4462501012d429d06dee316eebcbd
AVartem <artem100739@gmail.com>
2026-03-31 08:12:38 +0300

    lab2

---
+++ flatpages/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'templates/static_handler.html')
+def hello(request):
+    return HttpResponse(u'Привет, Мир!')
